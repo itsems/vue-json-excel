c07a9a1adc49f1841de8a507d7450fe520c6982d
emmalin <emma.lin@auo.com>
2022-01-13 14:15:02 +0800

    Add default func content
    Add empty default func content in beforeGenerate & beforeFinish as props func default setting

--- dist/vue-json-excel.esm.js
+++ dist/vue-json-excel.esm.js
@@ -228,10 +228,12 @@ var script = {
     //event before generate was called
     beforeGenerate: {
       type: Function,
+      default: () => {}
     },
     //event before download pops up
     beforeFinish: {
       type: Function,
+      default: () => {}
     },
     // Determine if CSV Data should be escaped
     escapeCsv: {
